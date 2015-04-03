38294a7f9f75700b89522636e9b37af26e5728f0
chiharum <m.chiharu0611@gmail.com>
2015-04-03 17:17:07 +0900

    コメントを書きました

--- app/src/main/java/com/example/chiharu_miyosi/test/MainActivity.java
+++ app/src/main/java/com/example/chiharu_miyosi/test/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    // 変えました
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
